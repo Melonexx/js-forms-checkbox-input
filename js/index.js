d48da601fd53a516bef9f351597f5ef957f5ec63
Eileen Ilsemann <eileenilsemanni@gmail.com>
2024-01-05 11:36:33 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
 
+hideTosError();
+
 function hideTosError() {
   tosError.setAttribute("hidden", "");
 }
@@ -16,9 +18,15 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  if (tosCheckbox.checked) {
+    // Wenn ja, verstecke die Fehlermeldung
+    hideTosError();
 
-  // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
-  alert("Form submitted");
+    // eslint-disable-next-line no-alert
+    alert("Form submitted"); // total optional, absolut nicht notwendig, kann man auch einfach weglassen
+  } else {
+    // Wenn nicht, zeige die Fehlermeldung an
+    showTosError();
+  }
 });
+// --^-- write your code here --^--
